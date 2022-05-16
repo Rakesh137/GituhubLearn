f63b50b11ae68cefa72adbcc2393379873f97ed9
Rakesh137 <chauhan.rakesh137@gmail.com>
2022-05-16 11:56:22 +0530

    Some thing changes

---
+++ app/src/main/java/chauhan/rakesh/gituhublearn/Test.java
@@ -0,0 +1,14 @@
+package chauhan.rakesh.gituhublearn;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Test extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
